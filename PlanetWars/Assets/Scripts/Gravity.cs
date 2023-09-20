b2ed203408e82c4661321303a7ba791b22f3e7c7
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2023-09-20 16:41:40 +1000

    RABABBABA
    Created Gravity

--- PlanetWars/Assets/Scripts/Gravity.cs
+++ PlanetWars/Assets/Scripts/Gravity.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
+     public float GravityForce;
      private void Update()
      {
-          
+          foreach (var rigidybody1 in FindObjectsByType<Rigidbody>(FindObjectsInactive.Exclude, FindObjectsSortMode.None))
+          {
+               rigidybody1.AddForce(transform.position - rigidybody1.transform.position * GravityForce);
+          }
      }
 }
