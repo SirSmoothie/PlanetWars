63936b9cf0b46f2faf73a67d06275b3e50e15609
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2023-09-20 15:48:08 +1000

    Created Gravity
    Cant move sun for some reason.

---
+++ PlanetWars/Assets/Scripts/Gravity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+     public float GravityForce;
+     private void Update()
+     {
+          foreach (var rigidybody1 in FindObjectsByType<Rigidbody>(FindObjectsInactive.Exclude, FindObjectsSortMode.None))
+          {
+               rigidybody1.AddForce(transform.position - rigidybody1.transform.position * GravityForce);
+          }
+     }
+}
