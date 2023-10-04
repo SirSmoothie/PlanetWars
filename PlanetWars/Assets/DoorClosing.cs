c3166413a97f4fc5ca7ce9055ca91bbe6cf4a110
Rory Rouillon <Thegameredbaron@gmail.com>
2023-10-05 00:53:42 +1100

    Door State Machine

--- PlanetWars/Assets/DoorClosing.cs
+++ PlanetWars/Assets/DoorClosing.cs
@@ -8,11 +8,13 @@ public class DoorClosing : MonoBehaviour
     public float closing;
     public DoorClosed closed;
     public GameObject topDoor;
+    public GameObject topDoorPos;
     public GameObject bottomDoor;
+    public GameObject bottomDoorPos;
     public float Speed;
     private void OnEnable()
     {
-        
+        StartDoorClosing();
     }
 
     private void Update()
@@ -21,13 +23,13 @@ public class DoorClosing : MonoBehaviour
         {
             DoorClosed();
         }
-        else
-        {
-            topDoor.transform.DOMove(new Vector3(transform.position.x, 0, transform.position.z), Speed);
-            bottomDoor.transform.DOMove(new Vector3(transform.position.x, 0, transform.position.z), Speed);
-        }
     }
 
+    void StartDoorClosing()
+    {
+        topDoor.transform.DOMove(new Vector3(bottomDoor.transform.position.x, transform.position.y, bottomDoor.transform.position.z), Speed);
+        bottomDoor.transform.DOMove(new Vector3(bottomDoor.transform.position.x, transform.position.y, bottomDoor.transform.position.z), Speed);
+    }
     void DoorClosed()
     {
         gameObject.GetComponent<DoorStateManager>().ChangeState(closed);
--- PlanetWars/Assets/DoorOpening.cs
+++ PlanetWars/Assets/DoorOpening.cs
@@ -9,11 +9,13 @@ public class DoorOpening : MonoBehaviour
     public float Opening;
     public DoorOpen opened;
     public GameObject topDoor;
+    public GameObject topDoorPos;
     public GameObject bottomDoor;
+    public GameObject bottomDoorPos;
     public float Speed;
     private void OnEnable()
     {
-        
+        StartDoorOpening();
     }
 
     public void Update()
@@ -22,16 +24,12 @@ public class DoorOpening : MonoBehaviour
         {
             DoorOpened();
         }
-        else
-        {
-            //topDoor.transform.DOMove(new Vector3(transform.position.x, 2.5f, transform.position.z), Speed);
-            //bottomDoor.transform.DOMove(new Vector3(transform.position.x, -2.5f, transform.position.z), Speed);
-        }
     }
 
-    void DoorOpen()
+    void StartDoorOpening()
     {
-        
+        topDoor.transform.DOMove(new Vector3(bottomDoor.transform.position.x, topDoorPos.transform.position.y, bottomDoor.transform.position.z), Speed);
+        bottomDoor.transform.DOMove(new Vector3(bottomDoor.transform.position.x, bottomDoorPos.transform.position.y, bottomDoor.transform.position.z), Speed);
     }
 
     void DoorOpened()
